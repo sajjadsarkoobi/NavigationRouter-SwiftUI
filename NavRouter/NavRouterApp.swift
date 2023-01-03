86aa35db2b6b85821ebcaaee0c659eb2ada035cd
Sajjad Sarkoobi <sajjadsarkoobi@gmail.com>
2023-01-03 12:49:33 +0300

    Initial Commit

---
+++ NavRouter/NavRouterApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavRouterApp.swift
+//  NavRouter
+//
+//  Created by Sajjad Sarkoobi on 3.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct NavRouterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
